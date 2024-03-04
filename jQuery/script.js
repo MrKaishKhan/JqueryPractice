b64832b2f77b41886a777b03d52cc814ea067676
MrKaishKhan <muhmmadkaishkhan@gmail.com>
2024-03-04 12:29:53 +0530

    try new but failed

---
+++ jQuery/script.js
@@ -0,0 +1,13 @@
+ 
+ 
+ // *************** 01 ***************
+ // $(document).ready(function(){
+
+//   // jQuery methods go here...
+
+// });
+$(document).ready(function(){
+    let a = $(".first").html();
+    console.log(a)
+});
+
