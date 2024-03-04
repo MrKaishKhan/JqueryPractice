262fe6190cfc999d2d51a72083ad1276837d0911
MrKaishKhan <muhmmadkaishkhan@gmail.com>
2024-03-04 16:24:47 +0530

    keycaps

--- jQuery/script.js
+++ jQuery/script.js
@@ -22,3 +22,38 @@ $(document).ready(function(){
 
 
 // *********** 03 selector ***************
+ $(document).ready(function(){
+    $(".top-01").click(function(){
+    $(".top-01").css("background-color", "blue");
+    });
+
+    $(".top-01").dblclick(function(){
+    $(".top-01").css("background-color", "gray");
+    });
+
+    // right click
+    $(".top-01").contextmenu(function(){
+    $(".top-01").css("background-color", "purple");
+    });
+
+
+    $(".top-01").mouseenter(function(){
+    $(".top-01").css("background-color", "red");
+    });
+
+
+    $(".top-01").mouseleave(function(){
+    $(".top-01").css("background-color", "white");
+    });
+
+ })
+ 
+ // *********** 04 selector ***************
+ // keyboard events file****************
+
+
+
+
+
+
+    
