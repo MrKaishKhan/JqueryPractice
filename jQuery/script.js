874f44d834334986d055b33a18200a71be2d81d9
MrKaishKhan <muhmmadkaishkhan@gmail.com>
2024-03-04 15:39:51 +0530

    mouseEvent

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,13 +1,24 @@
  
  
  // *************** 01 ***************
- // $(document).ready(function(){
+ // 2. $(document).ready(function(){});
+ // 1. $(function(){}) first load then show the element...
 
 //   // jQuery methods go here...
 
-// });
+// 
 $(document).ready(function(){
     let a = $(".first").html();
+   
     console.log(a)
 });
 
+
+// *********** 02 selector ***************
+
+$(document).ready(function(){
+    $("li:eq(2)").css("color", "red");
+});
+
+
+// *********** 03 selector ***************
